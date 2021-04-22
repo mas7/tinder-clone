902e222e542fc716ffcac9b24486b3d49669df36
MaS <mas@MaSs-MacBook-Pro.local>
2021-04-22 06:25:31 +0300

    Finished the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./Header.css";
 import PersonIcon from "@material-ui/icons/Person";
+import IconButton from "@material-ui/core/IconButton";
+import ForumIcon from "@material-ui/icons/Forum";
 
 const Header = () => {
   return (
     <div className="header">
-      <h2>Header</h2>
+      <IconButton>
+        <PersonIcon fontSize="large" className="header__icon" />
+      </IconButton>
+      <img
+        className="header__logo"
+        src="https://i.imgur.com/mfC81W6.png"
+        alt="Tinder Logo"
+      />
+      <IconButton>
+        <ForumIcon fontSize="large" className="header__icon" />
+      </IconButton>
     </div>
   );
 };
