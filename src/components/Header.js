abeb462e802d7505cda27744d4507a2eb019edbe
MaS <mas@MaSs-MacBook-Pro.local>
2021-04-22 05:52:26 +0300

    Basic setup + adding the header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h2>Header</h2>
+    </div>
+  );
+};
+
+export default Header;
